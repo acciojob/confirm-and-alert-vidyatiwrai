9fc67621d4e808bc89b671c69655428f9a2237ce
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-05-03 14:39:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// //your JS code here. If required.function save(){
 
-function save(){	 
-		  var userPreference;
+
  
 			if (confirm("You clicked OK. proceeding...") == true) {
 				userPreference = "Data saved successfully!";
